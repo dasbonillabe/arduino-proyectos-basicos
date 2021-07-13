f69ee7067643929db27e33eb8e233955114738bf
dasbonillabe <iam.santiago@hotmail.com>
2021-07-13 17:38:04 -0500

    Version final del proyecto 02

--- 02-Love-o-meter/02-Love-o-meter.ino
+++ 02-Love-o-meter/02-Love-o-meter.ino
@@ -1,7 +1,7 @@
 // Autor: Santiago Bonilla
 
 const int sensorPin = A0;
-const float baselineTemp = 20.0;
+const float baselineTemp = 19.0;
 
 void setup() {
   Serial.begin(9600);  // Abre un puerto en serie 9600 bits por segundo
@@ -17,25 +17,25 @@ void loop() {
   Serial.print(sensorVal);
 
   // Convierte la lectura del ADC a voltaje
-  float voltage = sensorVal*(5.0/1024);
+  float voltage = (sensorVal/1024.0)*5.0;
   Serial.print(" , Volts: ");
   Serial.print(voltage);
 
   // Convierte el voltaje a temperatura en grados
-  // float temperature = (voltage - 0.5)*100.0;
-  float temperature = 36.0*(voltage - 5.0)+125.0;
+  float temperature = 100.0*voltage - 50.0;
   Serial.print(", degrees C: ");
   Serial.println(temperature);
 
   if(temperature < baselineTemp){
     digitalWrite(2,LOW);
     digitalWrite(3,LOW);
-    digitalWrite(4,LOW);
-  } else if(temperature >= baselineTemp+2 && temperature < baselineTemp+4) {
+    digitalWrite(4,LOW);  
+  }
+  if((temperature >= baselineTemp) && (temperature < baselineTemp+3)){
     digitalWrite(2,HIGH);
     digitalWrite(3,LOW);
-    digitalWrite(4,LOW);    
-  } else if(temperature >= baselineTemp+4 && temperature < baselineTemp+6) {
+    digitalWrite(4,LOW);  
+  } else if((temperature >= baselineTemp+3) && (temperature < baselineTemp+6)) {
     digitalWrite(2,HIGH);
     digitalWrite(3,HIGH);
     digitalWrite(4,LOW);    
